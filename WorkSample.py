d15838a63904b20f38f57f54e17298d12db84f4e
Hannah <hkeim4321@gmail.com>
2022-04-10 17:20:42 -0500

    Update WorkSample.py

--- WorkSample.py
+++ WorkSample.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 import streamlit as st
 import base64
-
-
-# In[4]:
 
 
 #####################
@@ -19,17 +10,11 @@ st.write('''
 ''')
 
 
-# In[19]:
-
-
 def show_pdf(file_path):
     with open(file_path,"rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-# In[22]:
 
 
 st.markdown('## Data Cleaning', unsafe_allow_html=True)
@@ -53,16 +38,19 @@ with col2:
     st.button('Close PDF',key='4')
 
 
-# In[23]:
-
-
 st.markdown('## Multiple Linear Regression', unsafe_allow_html=True)
+st.write('''
+##### *Report* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='5'):            
         show_pdf("Multiple Linear Regression Report.pdf")
 with col2:
     st.button('Close PDF',key='6') 
+st.write('''
+##### *Code* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='7'):            
@@ -71,16 +59,19 @@ with col2:
     st.button('Close PDF',key='8')
 
 
-# In[24]:
-
-
 st.markdown('## Logistic Regression', unsafe_allow_html=True)
+st.write('''
+##### *Report* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='9'):            
         show_pdf("Logistic Regression Report.pdf")
 with col2:
-    st.button('Close PDF',key='10') 
+    st.button('Close PDF',key='10')
+st.write('''
+##### *Code* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='11'):            
@@ -89,16 +80,19 @@ with col2:
     st.button('Close PDF',key='12')
 
 
-# In[25]:
-
-
 st.markdown('## KNN Classification', unsafe_allow_html=True)
+st.write('''
+##### *Report* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='13'):            
         show_pdf("KNN Classification Report.pdf")
 with col2:
     st.button('Close PDF',key='14') 
+st.write('''
+##### *Code* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='15'):            
@@ -107,16 +101,19 @@ with col2:
     st.button('Close PDF',key='16')
 
 
-# In[26]:
-
-
 st.markdown('## K-means Clustering', unsafe_allow_html=True)
+st.write('''
+##### *Report* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='17'):            
         show_pdf("K-means Clustering.pdf")
 with col2:
     st.button('Close PDF',key='18') 
+st.write('''
+##### *Code* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='19'):            
@@ -125,16 +122,19 @@ with col2:
     st.button('Close PDF',key='20')
 
 
-# In[27]:
-
-
 st.markdown('## Principal Component Analysis', unsafe_allow_html=True)
+st.write('''
+##### *Report* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='21'):            
         show_pdf("Principal Component Analysis Report.pdf")
 with col2:
-    st.button('Close PDF',key='22') 
+    st.button('Close PDF',key='22')
+st.write('''
+##### *Code* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='23'):            
@@ -143,16 +143,19 @@ with col2:
     st.button('Close PDF',key='24')
 
 
-# In[28]:
-
-
 st.markdown('## Random Forest Classifier', unsafe_allow_html=True)
+st.write('''
+##### *Report* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='25'):            
         show_pdf("Random Forest Classifier Report.pdf")
 with col2:
-    st.button('Close PDF',key='26') 
+    st.button('Close PDF',key='26')
+st.write('''
+##### *Code* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='27'):            
@@ -161,16 +164,19 @@ with col2:
     st.button('Close PDF',key='28')
 
 
-# In[29]:
-
-
 st.markdown('## Neural Network', unsafe_allow_html=True)
+st.write('''
+##### *Report* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='29'):            
         show_pdf("Recurrent Neural Network for Sentiment Analysis.pdf")
 with col2:
-    st.button('Close PDF',key='30') 
+    st.button('Close PDF',key='30')
+st.write('''
+##### *Code* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='31'):            
@@ -179,25 +185,25 @@ with col2:
     st.button('Close PDF',key='32')
 
 
-# In[30]:
-
-
 st.markdown('## Time Series Analysis', unsafe_allow_html=True)
+st.write('''
+##### *Report* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='33'):            
         show_pdf("Time Series Analysis Report.pdf")
 with col2:
-    st.button('Close PDF',key='34') 
+    st.button('Close PDF',key='34')
+st.write('''
+##### *Code* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='35'):            
         show_pdf("Time Series Analysis Code.pdf")
 with col2:
     st.button('Close PDF',key='36')
-
-
-# In[31]:
 
 
 st.markdown('## Market Basket Analysis', unsafe_allow_html=True)
@@ -209,26 +215,22 @@ with col2:
     st.button('Close PDF',key='38') 
 
 
-# In[32]:
-
-
 st.markdown('## Chi-Square Capstone', unsafe_allow_html=True)
+st.write('''
+##### *Analysis* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='39'):            
         show_pdf("Chi-Square Analyses of New York City Business Licenses.pdf")
 with col2:
-    st.button('Close PDF',key='40') 
+    st.button('Close PDF',key='40')
+st.write('''
+##### *Executive Report* 
+''')
 col1, col2= st.columns(2)
 with col1:  
     if st.button('Read PDF',key='41'):            
         show_pdf("Chi-Square Analysis Executive Summary.pdf")
 with col2:
     st.button('Close PDF',key='42')
-
-
-# In[ ]:
-
-
-
-
