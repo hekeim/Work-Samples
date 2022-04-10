d149424d01c24b3c0c1a969635a24bdc0af2938f
Hannah <hkeim4321@gmail.com>
2022-04-10 16:05:55 -0500

    Update WorkSample.py

--- WorkSample.py
+++ WorkSample.py
@@ -25,7 +25,7 @@ st.write('''
 def st_display_pdf(pdf_file):
     with open(pdf_file,"rb") as f:
       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = F'<embed src=”data:application/pdf;base64,{base64_pdf}” width=”700″ height=”1000″ type=”application/pdf”>'
+    pdf_display = F'<iframe src=”data:application/pdf;base64,{base64_pdf}” width=”700″ height=”1000″ type=”application/pdf”>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
